4c46bad140fb6cc390d6db3ba70b16617308586c
msafayat <msafayat@gmail.com>
2021-06-05 09:22:43 -0400

    test

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>Hello, World! My First Application</h1>"
+@app.route("/pages")
+def thanks():
+    return render_template('thanks.html')
